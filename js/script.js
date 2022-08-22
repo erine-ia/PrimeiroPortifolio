dc31829b7b4f4500f4b0927932f620e9f7b20834
erineia <erineia@live.com>
2022-08-22 16:28:55 -0300

    Atualizzação de media queries

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+const $nav = document.querySelector('.menu--fechado');
+const $btn = document.querySelector(".btn")
+$btn.addEventListener('click', abrir, false)
+
+function abrir(){
+    if($nav.classList.contains("menu--fechado") && $btn.classList.contains("btn")){
+        $nav.classList.remove("menu--fechado");
+        $nav.classList.add("menu")
+
+        $btn.classList.remove("btn");
+        $btn.classList.add("btn-cruz")
+
+
+    }else{
+        $nav.classList.add("menu--fechado");
+        $nav.classList.remove("menu")
+
+        $btn.classList.add("btn");
+        $btn.classList.remove("btn-cruz")
+
+    }
+   
+}
+
+
+
+    
 
 
 
